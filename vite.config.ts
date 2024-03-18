42fe03e864c78703dbb157c28334972a82a29030
Mr.long <466419162@qq.com>
2024-03-18 22:16:43 +0800

    第二版编码器

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
+    base: "./", //请勿删除此语句，否则将导致部署失败
     plugins: [vue()],
     server: {
         host: "0.0.0.0",
     },
 });
-=======
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
->>>>>>> 6a733d0f1474aa01e257045d35997f997020f58d
